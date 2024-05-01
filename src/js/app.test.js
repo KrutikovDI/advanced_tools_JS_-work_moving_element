b653e8f6401fc4bd1f6e34f52bb4b84415b3c902
Danil Krutikov <kdanil.i@yandex.ru>
2024-05-01 13:43:25 +0700

    Correct test

--- src/js/app.test.js
+++ src/js/app.test.js
@@ -3,5 +3,5 @@ import Goblin from "./main/main";
 test("проверяем создание экземпляра класса", () => {
   const goblin = new Goblin();
   const correct = "0";
-  expect(goblin.missCounter.textContent).toEqual(correct);
+  expect(goblin).toBeInstanceOf(Goblin);
 });
